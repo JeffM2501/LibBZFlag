13bdfb955f3490769224a81055edaddbde20b891
JeffM2501 <JeffM2501@gmail.com>
2016-11-19 10:12:48 -0800

    server stuff

---
+++ BZFlag.Networking.Server/BanList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BZFlag.Networking
+{
+	public class BanList
+	{
+		public class BanRecord
+		{
+			public string BanMask = string.Empty;
+			public string Reason = string.Empty;
+			public string Source = string.Empty;
+		}
+
+		protected List<BanRecord> Hostbans = new List<BanRecord>();
+		protected List<BanRecord> IPBans = new List<BanRecord>();
+
+		public BanRecord FindHostBan(string host)
+		{
+			string lHost = host.ToLowerInvariant();
+			// just do a simple lookup now
+			return Hostbans.Find(x => lHost.Contains(x.BanMask));
+		}
+
+		public BanRecord FindIPBan(string ip)
+		{
+			string lip = ip.ToLowerInvariant();
+			// just do a simple lookup now
+			return IPBans.Find(x => lip.Contains(x.BanMask));
+		}
+	}
+}
---
+++ BZFlag.Networking.Server/TCPConnectionManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BZFlag.Networking
+{
+	public class TCPConnectionManager
+	{
+		public int Port = 5154;
+		public TcpListener Listener = null;
+
+		public class PendingClient
+		{
+			public TcpClient ClientConnection = null;
+
+			public bool DNSStarted = false;
+			public IPHostEntry HostEntry = null;
+
+			public bool DNSPassed = false;
+
+			public bool IPStarted = false;
+			public bool IPPassed = false;
+
+			public bool DataRecieved = false;
+			public bool ProtcolPassed = false;
+
+			public byte[] PendingData = new byte[0];
+		}
+		protected List<PendingClient> PendingClients = new List<PendingClient>();
+
+		protected Thread WorkerThread = null;
+
+		public TCPConnectionManager(int port)
+		{
+			Port = port;
+			Listener = new TcpListener(IPAddress.Any, port);
+		}
+
+		public void StartUp()
+		{
+			Listener.BeginAcceptTcpClient(TCPClientAccepted, null);
+		}
+
+		public void Shutdown()
+		{
+			if(WorkerThread != null)
+				WorkerThread.Abort();
+
+			WorkerThread = null;
+		}
+
+		protected void TCPClientAccepted(IAsyncResult ar)
+		{
+			PendingClient c = new PendingClient();
+			c.ClientConnection = Listener.EndAcceptTcpClient(ar);
+			Listener.BeginAcceptTcpClient(TCPClientAccepted, null);
+
+			lock(PendingClients)
+				PendingClients.Add(c);
+
+			if (WorkerThread == null)
+			{
+				WorkerThread = new Thread(new ThreadStart(ProcessPendingClients));
+				WorkerThread.Start();
+			}
+		}
+
+		protected void ProcessPendingClients()
+		{
+			PendingClient[] clients = null;
+
+			lock(PendingClients)
+				clients = PendingClients.ToArray();
+
+			while (clients.Length > 0)
+			{
+				foreach(PendingClient c in clients )
+				{
+					if (!c.DNSStarted)
+					{
+						var address = ((IPEndPoint)c.ClientConnection.Client.RemoteEndPoint).Address.ToString();
+						Dns.BeginGetHostEntry(address, DNSLookupCompleted, c);
+						c.DNSStarted = true;
+					}
+					else if (!c.DNSPassed)
+					{
+						if(c.HostEntry != null)
+						{
+							// lookup the host in the ban list
+						}
+					}
+				}
+
+				Thread.Sleep(100);
+				lock(PendingClients)
+					clients = PendingClients.ToArray();
+			}
+
+			WorkerThread = null;
+		}
+
+		protected void DNSLookupCompleted(IAsyncResult ar)
+		{
+			var results = Dns.EndGetHostEntry(ar);
+			PendingClient c = ar.AsyncState as PendingClient;
+			if(c == null)
+				return;
+
+			c.HostEntry = results;
+		}
+
+	}
+}
